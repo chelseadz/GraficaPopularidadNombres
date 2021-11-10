fe18e33d4763da051ccf9cab0ba50ac87f5d9c98
DIEGO TORRES GONZALEZ <a220206322@unison.mx>
2021-11-10 14:46:24 -0700

    Add functionality to Name class

--- src/Name.java
+++ src/Name.java
@@ -1,6 +1,30 @@
 import java.lang.String;
 
 public class Name {
-    String name;
+    private String name;
+    private boolean sex; // false es femenino y true masculino.
+    private int[] popularityByDecade;
+
+    Name(String name, boolean sex, int[] popularity) {
+        this.name = name;
+        this.sex = sex;
+        this.popularityByDecade = popularity;
+    }
+
+    public int[] getPopularityByDecade() {
+        return popularityByDecade;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int compareTo(Name b) {
+        return this.name.compareTo(b.name);
+    }
 
 }
