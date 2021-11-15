cc3270636eeb41378e4578c0b7d3bbfea4676da8
DIEGO TORRES GONZALEZ <a220206322@unison.mx>
2021-11-14 21:14:53 -0700

    Terminar graficado y detalles.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class MainWindow extends JFrame {
     private JPanel pMain;
@@ -15,12 +18,13 @@ public class MainWindow extends JFrame {
     private JPanel pControl;
     private final PaintGraphic graphic;
 
-    String input;
-    String formattedName;
-    String selectedSex;
-    Name searchName;
-    ArrayList<Name> names = GetNames("names.txt");
-    Integer[] searchNamePopu;
+    private String input;
+    private String formattedName;
+    private String selectedSex;
+    private Name searchName;
+    private ArrayList<Name> names = GetNames("names.txt");
+    private Integer[] searchNamePopu;
+    private ButtonGroup sButtonGroup;
 
     private final int WIDTH = 1000, HEIGHT = 550;
 
@@ -40,7 +44,7 @@ public class MainWindow extends JFrame {
 
         fRadioButton.setActionCommand("F");
         mRadioButton.setActionCommand("M");
-        ButtonGroup sButtonGroup = new ButtonGroup();
+        sButtonGroup = new ButtonGroup();
         sButtonGroup.add(fRadioButton);
         sButtonGroup.add(mRadioButton);
 
@@ -48,30 +52,20 @@ public class MainWindow extends JFrame {
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                input = tfSearchName.getText().trim();
-                formattedName = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
-                selectedSex = sButtonGroup.getSelection().getActionCommand();
-
-                try {
-                    int searchResult = names.indexOf(new Name(formattedName, selectedSex, new Integer[0]));
-
-                    if (searchResult == -1) JOptionPane.showMessageDialog(null, "No se encontró el nombre ingresado.",
-                            "Error", JOptionPane.ERROR_MESSAGE);
-                    else {
-                        searchName = names.get(searchResult);
-                        searchNamePopu = searchName.getPopularityByDecade();
-                        graphic.setList(searchNamePopu);
-                        //graphic.paintComponent(new Graphics());
-                    }
-                }
-                catch (Exception StringIndexOutOfBoundsException) {
-                    JOptionPane.showMessageDialog(null, "Entrada vacía. Por favor ingresa un nombre.",
-                            "Error", JOptionPane.ERROR_MESSAGE);
-                }
+                paintGraph();
             }
         });
 
+        tfSearchName.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+
+                if (e.getKeyCode() == 10 ) { // Enter pressed
+                    paintGraph();
+                }
+            }
+        });
     }
 
     public ArrayList<Name> GetNames(String pathname){
@@ -102,4 +96,28 @@ public class MainWindow extends JFrame {
         return names;
     }
 
+    private void paintGraph() {
+
+        input = tfSearchName.getText().trim();
+        formattedName = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+        selectedSex = sButtonGroup.getSelection().getActionCommand();
+        try {
+            int searchResult = names.indexOf(new Name(formattedName, selectedSex, new Integer[0]));
+
+            if (searchResult == -1) JOptionPane.showMessageDialog( pMain, "No se encontró el nombre ingresado.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            else {
+                searchName = names.get(searchResult);
+                searchNamePopu = searchName.getPopularityByDecade();
+                graphic.setList(searchNamePopu);
+                graphic.repaint();
+                graphic.setVisible(false);
+                graphic.setVisible(true);
+            }
+        }
+        catch (Exception StringIndexOutOfBoundsException) {
+            JOptionPane.showMessageDialog(null, "Entrada vacía. Por favor ingresa un nombre.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
